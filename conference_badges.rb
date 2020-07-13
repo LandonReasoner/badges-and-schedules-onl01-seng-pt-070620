7b1a6702623e11019f60c3f6e121fc49f0a7a333
Landon <reasoner.landon90@gmail.com>
2020-07-13 23:34:03 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,6 +3,6 @@ def badge_maker(name)
  "Hello, my name is #{name}."
 end
 
-def batch_badge_creator(attendees)
-  attendees.map{ |attendees| badge_maker(name)}
+def batch_badge_creator(name)
+  name.map{ |attendees| badge_maker(name)}
 end
