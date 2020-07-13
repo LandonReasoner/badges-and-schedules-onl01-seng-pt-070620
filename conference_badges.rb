0f691ade95697759edaa2e7437c4c7ce13a5e5b0
Landon <reasoner.landon90@gmail.com>
2020-07-13 23:45:07 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,6 +3,13 @@ def badge_maker(name)
  "Hello, my name is #{name}."
 end
 
-def batch_badge_creator(name)
-  name.map{ |name| badge_maker(name)}
+def batch_badge_creator(names)
+  names.map{ |name| badge_maker(name)}
+end
+
+def assign_rooms(attendees)
+  room_assignments = []
+  attendees.each_with_index do |name, i|
+    room_assignments << "Hello, #{name}! You'll be assigned to room #{i +1}!"
+  end 
 end
